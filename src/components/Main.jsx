ba26f37ad86631af8c61370f526e1ccf45a17193
rohilpinto <rohilcrispinto@gmail.com>
2021-07-09 16:26:35 +0530

    initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+
+// for the accordion
+import ArrowDown from "@material-ui/icons/KeyboardArrowDownSharp";
+import ArrowUp from "@material-ui/icons/KeyboardArrowUpSharp";
+
+const Main = () => {
+  return (
+    <div className="Main">
+      <div className="outer-card">
+        <div className="accordion-parent">
+          <div className="question">Do I have to allow the use of cookes?</div>
+          <button>+</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
