e27bcee2a5083234110309b6c201e0ca1cc81539
rohilpinto <rohilcrispinto@gmail.com>
2021-07-12 15:12:25 +0530

    added styles and logic

---
+++ src/components/Content.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+import ArrowDown from "@material-ui/icons/KeyboardArrowDownSharp";
+import ArrowUp from "@material-ui/icons/KeyboardArrowUpSharp";
+
+const Content = ({ content }) => {
+  const [isTrue, setIsTrue] = useState(false);
+
+  return (
+    <div className="accordion-wrapper">
+      <div className="content-wrapper">
+        <div className="question">{content.question} </div>
+        <div className="answer"> {isTrue && <div>{content.answer}</div>}</div>
+      </div>
+      <div className="arrow-wrapper">{isTrue ? <ArrowUp onClick={() => setIsTrue(!isTrue)} /> : <ArrowDown onClick={() => setIsTrue(!isTrue)} />}</div>
+    </div>
+  );
+};
+
+export default Content;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 // import Grid from "@material-ui/core/Grid";
 // import Paper from "@material-ui/core/Paper";
 
 // for the accordion
-// import ArrowDown from "@material-ui/icons/KeyboardArrowDownSharp";
-// import ArrowUp from "@material-ui/icons/KeyboardArrowUpSharp";
+import ArrowDown from "@material-ui/icons/KeyboardArrowDownSharp";
+import ArrowUp from "@material-ui/icons/KeyboardArrowUpSharp";
+import data from "../data";
+import Content from "./Content";
+// console.log(data);
 
 const Main = () => {
   return (
     <div className="Main">
       <div className="outer-card">
-        <div className="accordion-parent">
-          <div className="question">Do I have to allow the use of cookes?</div>
-          <button>+</button>
+        <div className="title-container">
+          <h1>Questions And Answers About Login</h1>
+        </div>
+        <div className="accordion-container">
+          {data.map((content) => {
+            return <Content content={content} key={content.id} />;
+          })}
         </div>
       </div>
+      <div className="accordion-content-parent"></div>
     </div>
   );
 };
